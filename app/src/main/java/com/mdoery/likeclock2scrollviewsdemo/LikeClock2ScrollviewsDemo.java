276143a04242140a6c5493e65ce21fa7d9b91fe6
Marya Doery <mdoery@gmail.com>
2018-11-05 20:32:13 -0500

    Initial (and complete) simple demo project that shows how to add a vertically scrolling set of TextViews to a panel in your app.

---
+++ app/src/main/java/com/mdoery/likeclock2scrollviewsdemo/LikeClock2ScrollviewsDemo.java
@@ -0,0 +1,13 @@
+package com.mdoery.likeclock2scrollviewsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LikeClock2ScrollviewsDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
